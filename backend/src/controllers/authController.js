a4bfcea83a356cd0a3aa5ebd79bcbe231e5ad159
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-05-06 22:44:26 +0530

    Added: Auth Middleware and Auth Controller

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,24 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ email, password: hashed });
+
+  res.json({ token: generateToken(user._id) });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  res.json({ token: generateToken(user._id) });
+};
