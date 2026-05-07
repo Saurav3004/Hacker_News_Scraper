6c5a234db04051cee27ff8d43c9ee2cc1d0b0986
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-05-07 14:06:09 +0530

    fix cors bugs

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: "https://hacker-news-scraper.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
